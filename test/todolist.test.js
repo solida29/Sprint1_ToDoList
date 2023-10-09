003b3c03c4ec98edc513b51841be9d2870957158
Solida <solida.var@gmail.com>
2023-10-09 13:47:12 +0200

    exports para Jest

--- test/todolist.test.js
+++ test/todolist.test.js
@@ -1,11 +1,14 @@
 const { searching } = require('../dist/index');
 const { add } = require('../dist/index');
+const { list } = require('../dist/index')
 
 test('buscar posicion en el array', () => {
     expect(searching('poner la lavadora')).toBe(0);
   });
 
 test('agregar tarea', () => {
-    expect(add('sacar al perro')).toBe('Task added successfully');
+    add('sacar al perro');
+    expect(list[list.length - 1].task).toBe('sacar al perro');
   });
+
 
